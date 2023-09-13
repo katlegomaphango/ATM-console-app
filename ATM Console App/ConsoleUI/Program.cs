b1a0796c52bcd9cae2d630273802056b49d26500
katlegomaphango <mphangokatlego08@gmail.com>
2023-09-13 22:52:25 +0200

    menu

--- ATM Console App/ConsoleUI/Program.cs
+++ ATM Console App/ConsoleUI/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        char menuKey = Menu();
+        
+    }
+
+    private static char Menu()
+    {
+        Console.WriteLine("\n\n\t\tATM Console Application: Welcome...");
+        Console.WriteLine("\n\n\t\tPlease choose an option ");
+        Console.WriteLine("\n\t\t1: Deposit");
+        Console.WriteLine("\t\t2: Withdraw");
+        Console.WriteLine("\t\t3: Show Balance");
+        Console.Write("\t\t4: Exit");
+
+        return Console.ReadKey().KeyChar;
+    }
+}
